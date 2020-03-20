dbba642f453fd44a3edcdc3d7fad3aa67883acef
xikasan <xika0907@gmail.com>
2020-03-21 01:01:32 +0900

    update model

--- fdair/models/model.py
+++ fdair/models/model.py
@@ -16,10 +16,13 @@ class FaultDetector(tk.Model):
     ):
         super().__init__(name=name)
 
-        assert len(units) == 3
+        assert len(units) == 2
         self.l1 = tk.layers.Dense(units[0], activation="relu", name="L1")
+        self.b1 = tk.layers.BatchNormalization()
         self.l2 = tk.layers.Dense(units[1], activation="relu", name="L2")
-        self.l3 = tk.layers.Dense(units[2], activation="relu", name="L3")
+        self.b2 = tk.layers.BatchNormalization()
+        # self.l3 = tk.layers.Dense(units[2], activation="relu", name="L3")
+        # self.b3 = tk.layers.BatchNormalization()
         self.l4 = tk.layers.Dense(dim_output, name="L4")
 
         dummy_input = tk.Input((dim_input,), dtype=tf.float32)
@@ -27,8 +30,12 @@ class FaultDetector(tk.Model):
 
     def call(self, inputs):
         feature = self.l1(inputs)
+        feature = self.b1(feature)
         feature = self.l2(feature)
-        feature = self.l3(feature)
+        feature = self.b2(feature)
+        # feature = tf.concat([inputs, feature], axis=-1)
+        # feature = self.l3(feature)
+        # feature = self.b3(feature)
         feature = self.l4(feature)
         return feature
 
